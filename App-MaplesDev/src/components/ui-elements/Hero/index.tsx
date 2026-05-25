0c4c8c5bf44b3fcdaaca0143d34b35df07f31233
Girish <gnischal37@7oaks.org>
2026-05-24 21:53:54 -0500

    changed the structure of the Hero component

--- App-MaplesDev/src/components/ui-elements/Hero/index.tsx
+++ App-MaplesDev/src/components/ui-elements/Hero/index.tsx
@@ -1,67 +1,36 @@
-import { useEffect, useState } from "react";
-
 export const Hero = () => {
-    const text: string = "is about network, collaboration, and fun";
-    const mdev: string = "maples{dev}"
-    
-    const [width, setWidth] = useState(window.innerWidth);
-    
-    useEffect(() => {
-        const handleResize = () => {
-            setWidth(window.innerWidth);        
-        };
-        
-        window.addEventListener("resize", handleResize);
-        
-        return () => {
-            window.removeEventListener("resize", handleResize);
-        };
-    
-    }, []);  
-      
-    // breakpoints
-    const LG = 1920;
-    const MD = 480;
-    
-    const k = Math.min(
-        1,
-        Math.max(
-            0,
-            (LG - width) / (LG - MD)
-        )
-    );
-    
-    const indent = 630 - (455 * k);
-    const left = 100 - (90 * k);
-    
-    return(
-        <div className="relative">
+    return (
+        <div className="relative w-full">
             <div
                 className="
-                    relative
-                    text-[clamp(2rem,6vw,10rem)] 
-                    text-center
-                    text-pretty
-                    font-medium 
-                    flex-col 
-                    items-center 
-                    justify-center
-                "
-                style={{
-                    textIndent: `${indent}px`,
-                }}
-            >{ text }</div>
-            
-            <div className="
-                    absolute
-                    top-[0px]
                     text-[clamp(2rem,6vw,10rem)]
                     font-medium
+                    text-center
+                    leading-none
                 "
-                style={{
-                    left: `${left}px`,
-                }}
-            >{ mdev }</div>
+            >
+                <span className="invisible">
+                    maples{`{dev}`}
+                </span>
+                <span>
+                    is about network, collaboration, and fun
+                </span>
+            </div>
+            <div
+                className="
+                    absolute
+                    top-0
+                    left-[62%]
+                    text-[clamp(2rem,6vw,10rem)]
+                    font-medium
+                    leading-none
+                    hero-text-animation
+                    will-change-transform
+                "
+                
+            >
+                maples{`{dev}`}
+            </div>
         </div>
     );
 };
