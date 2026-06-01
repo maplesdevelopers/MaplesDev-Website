33c1c7d581d4809de664ed55d0a3451bb533584b
Girish <gnischal37@7oaks.org>
2026-06-01 08:57:43 -0500

    changed the app layout and added smooth scrolling

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,12 @@ import { Hero } from './components/ui-elements/Hero';
 import { Footer } from './components/ui-elements/Footer';
 import { NetworkContent, CollaborationContent, FunContent } from './components/ui-elements/sections';
 
+import { Link, Element } from 'react-scroll';
+
 function App() {
     return (
         <>
-            <div
-                className="m-0 p-0 box-border !scroll-smooth"
-            >
+            <div className="m-0 p-0 box-border !scroll-smooth">
                 <section
                     className="relative py-[10vw] px-[30vw] min-w-full min-h-dvh m-0 flex flex-col place-content-center box-border wrap-break-word items-center justify-center" 
                     id="intro"
@@ -16,33 +16,33 @@ function App() {
                     <p className="text-[clamp(1rem,1vw,1.6rem)] absolute bottom-10">[scroll]</p>
                 </section>
                 
-                {/* Navigation */}                
-                <section
-                    className="min-h-[80dvh] min-w-full items-center justify-center"
+                <nav
+                    className="min-h-[100dvh] min-w-full items-center justify-center flex place-content-center gap-[1rem] text-[clamp(1rem,1vw,1.6rem)]"
                 >
-                    <div className="flex place-content-center mt-[40dvh] gap-[1rem] text-[clamp(1rem,1vw,1.6rem)]">
-                        <a 
-                            href="#network" 
-                            className="border-[0.1rem] p-[0.5rem]"
-                        >network</a>
-                        <a href="#collaboration" className="border-[0.1rem] p-[0.5rem]">collaboration</a>
-                        <a href="#fun" className="border-[0.1rem] px-[1rem] py-[0.5rem]">fun</a>
-                    </div>
-                </section>
+                    <Link to="network" smooth={true} duration={500} className="border-[0.1rem] p-[0.5rem] select-none hover:cursor-pointer">
+                        network
+                    </Link>
+                    <Link to="collaboration" smooth={true} duration={500} className="border-[0.1rem] p-[0.5rem] select-none hover:cursor-pointer">
+                        collaboration
+                    </Link>
+                    <Link to="fun" smooth={true} duration={500} className="border-[0.1rem] px-[1rem] py-[0.5rem] select-none hover:cursor-pointer">
+                        fun
+                    </Link>
+                </nav>
                 
-                <section className="relative min-h-dvh min-w-screen flex flex-col box-border items-center justify-center" id="network">
+                <Element className="relative min-h-dvh min-w-screen flex flex-col box-border items-center justify-center" name="network">
                     <NetworkContent />
                     {/* Spacer */}
                     <div className="min-w-screen min-h-[50dvh]"></div>
-                </section>
+                </Element>
                 
-                <section className="min-h-dvh min-w-screen" id="collaboration">
+                <Element className="min-h-dvh min-w-screen" name="collaboration">
                     <CollaborationContent />
-                </section>
+                </Element>
                 
-                <section className="relative py-[10vw] px-[30vw] min-w-full min-h-dvh m-0 flex flex-col place-content-center box-border wrap-break-word items-center justify-center" id="fun">
+                <Element className="relative py-[10vw] px-[30vw] min-w-full min-h-dvh m-0 flex flex-col place-content-center box-border wrap-break-word items-center justify-center" name="fun">
                     <FunContent />
-                </section>
+                </Element>
                 
                 {/* Footer */}
                 <section className="relative flex place-content-center h-[10px] w-full mb-30">
